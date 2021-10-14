f287915a623e67e32e2522d25da236a620865fd9
Christian <crm1997@live.com>
2021-10-14 15:40:39 -0400

    First commit

---
+++ hello.sh
@@ -0,0 +1,19 @@
+#This is a comment to add information to your code. It will not run 
+#echo Comment
+echo Hello
+echo World
+
+msg= "Hello World"
+
+echo $msg
+
+echo what is your name?
+read userInput
+echo "Hello $userInput , Welcome to Programming"
+
+for var in 1 2 3 4 5
+do
+	echo $var
+	echo This variable currently holds: $var
+done
+
